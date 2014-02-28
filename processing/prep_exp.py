66c87b5dc847be48ba8aa8c4477ebfcc48596564
Michael Byrne <michael.byrne@fcc.gov>
2014-02-28 08:27:46 -0500

    minor changes

--- processing/prep_exp.py
+++ processing/prep_exp.py
@@ -136,7 +136,7 @@ def updateWorking(myST):
 		myCur.execute(fullSQL)
 	#myvalue of 7 is Water and not anything else 
 	mySQL = "UPDATE " + schema + ".working set myvalue = 7 "
-	mySQL = mySQL + " WHERE myvalue is null and water = 1; COMMIT; "
+	mySQL = mySQL + " WHERE water = 1; COMMIT; "
 	myCur.execute(mySQL)
 	#myvalue of 8 is Unpopulated and nothing else
 	mySQL = "UPDATE " + schema + ".working set myvalue = 8 "
@@ -163,13 +163,21 @@ try:
 	myConn = "dbname=" + db + " host=" + myHost + " port=" + myPort + " user=" + myUser
 	conn = psycopg2.connect(myConn)
 	mkOutTbl()
-	theStates = ["01","02","03","04","05","06","07","08","09"]	
-	theStates = ["08"]
+	theStates = ["01","02","04","05","06","08","09"] #03, 07
+	theStates = theStates + ["10","11","12","13","15","16","17","18","19"] #14
+	theStates = theStates + ["20","21","22","23","24","25","26","27","28","29"]	
+	theStates = theStates + ["30","31","32","33","34","35","36","37","38","39"]
+	theStates = theStates + ["40","41","42","44","45","46","47","48","49"] #43
+	theStates = theStates + ["50","51","53","54","55","56"] #"52"
+	theStates = theStates + ["60","66","69","72","78","47"] #50
+	theStates = ["27"]
+
 	for theST in theStates:
 		print "    begining working on State: " + theST
 		createWorking(theST)
 		updateWorking(theST)
-#		appendOutput()
+		print "    		...disolving..."
+		appendOutput()
 	now = time.localtime(time.time())
 	print "local time:", time.asctime(now)
 except:
